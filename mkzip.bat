c5bd92af93a120b07315c3439a498f8188cc88d8
Michael Casadevall <michael.casadevall@canonical.com>
2009-04-19 12:32:08 -0700

    Updated the release scripts to be more user-friendly

--- mkzip.bat
+++ mkzip.bat
@@ -1,7 +1,5 @@
 @echo off
 PATH C:\Program Files\WinZip;%PATH%
-cd ..\
-wzzip -r -p Win32DiskImager\win32diskimager-source.zip Win32DiskImager\
-cd Win32DiskImager
+bzr export win32diskimager-source.zip
 wzzip win32diskimager-binary.zip mingwm10.dll Win32DiskImager.exe QtGui4.dll QtCore4.dll GPL-2 LGPL-2.1 README.txt
 @echo on
