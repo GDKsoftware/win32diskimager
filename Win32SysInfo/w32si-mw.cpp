7404b5bc2f3bae0a2cc060a1ff1f7c4e306733ff
Tobin Davis <gruemaster@gmail.com>
2013-02-19 10:26:21 -0800

    Renamed files to avoid confusion.

---
+++ Win32SysInfo/w32si-mw.cpp
@@ -0,0 +1,14 @@
+#include "w32si.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
