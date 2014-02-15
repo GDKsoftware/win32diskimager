f041dc5f8b78d0171df877376bee868d062cc906
Tobin Davis <gruemaster@gmail.com>
2014-02-14 17:37:24 -0800

    Added Italian translation (LP#1270388)

---
+++ src/diskimager_it.ts
@@ -0,0 +1,398 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it_IT">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="17"/>
+        <source>Win32 Disk Imager</source>
+        <translation type="unfinished">Win32 Disk Imager</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="36"/>
+        <source>Image File</source>
+        <translation type="unfinished">File immagine</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="51"/>
+        <source>...</source>
+        <translation type="unfinished">...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="65"/>
+        <source>Device</source>
+        <translation type="unfinished">Dispoditivo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="122"/>
+        <source>If checked, will generate the MD5 hash for the image file</source>
+        <translation type="unfinished">Se spuntato genererà l&apos;hash MD5 per il file immagine</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="141"/>
+        <source>MD5 Hash: </source>
+        <translation type="unfinished">Hash MD5:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="164"/>
+        <source>Progress</source>
+        <translation type="unfinished">Progresso</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="182"/>
+        <source>%p%</source>
+        <translation type="unfinished">%p%</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="216"/>
+        <source>Cancel current process.</source>
+        <translation type="unfinished">Arresta il processo.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="219"/>
+        <source>Cancel</source>
+        <translation type="unfinished">Annulla</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="235"/>
+        <source>Read data from &apos;Device&apos; to &apos;Image File&apos;</source>
+        <translation type="unfinished">Leggi i dati dal &apos;Device&apos; e scrivili nel &apos;Image file&apos;</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="238"/>
+        <source>Read</source>
+        <translation type="unfinished">Leggi</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="251"/>
+        <source>Write data in &apos;Image File&apos; to &apos;Device&apos;</source>
+        <translation type="unfinished">Scrivi il file di immagine &apos;Image File&apos; su &apos;Device&apos;</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="254"/>
+        <source>Write</source>
+        <translation type="unfinished">Scrivi</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="267"/>
+        <source>Exit Win32 Disk Imager</source>
+        <translation type="unfinished">Esci da Win32 Disk Imager</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="270"/>
+        <source>Exit</source>
+        <translation type="unfinished">Esci</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="101"/>
+        <location filename="mainwindow.cpp" line="111"/>
+        <source>Exit?</source>
+        <translation type="unfinished">Esci?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="101"/>
+        <source>Exiting now will result in a corrupt image file.
+Are you sure you want to exit?</source>
+        <translation type="unfinished">Annullare ora creerà un file immagine corrotto.
+Sei sicuro di voler uscire?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="111"/>
+        <source>Exiting now will result in a corrupt disk.
+Are you sure you want to exit?</source>
+        <translation type="unfinished">Annullare ore creerà un disco corrotto.
+Sei sicuro di voler uscire?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="123"/>
+        <source>Select a disk image</source>
+        <translation type="unfinished">Seleziona un file immagine</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="148"/>
+        <source>Generating...</source>
+        <translation type="unfinished">Generazione...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="194"/>
+        <source>Cancel?</source>
+        <translation type="unfinished">Annulla?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="194"/>
+        <source>Canceling now will result in a corrupt destination.
+Are you sure you want to cancel?</source>
+        <translation type="unfinished">Annullare ora creerà una destinazione corrotta.
+Sei sicuro di voler annullare?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="244"/>
+        <location filename="mainwindow.cpp" line="324"/>
+        <location filename="mainwindow.cpp" line="444"/>
+        <location filename="mainwindow.cpp" line="526"/>
+        <source>Write Error</source>
+        <translation type="unfinished">Errore in scrittura</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="244"/>
+        <location filename="mainwindow.cpp" line="444"/>
+        <source>Image file cannot be located on the target device.</source>
+        <translation type="unfinished">Il file di immagine non può essere trovato sul device di destinazione.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="254"/>
+        <source>Confirm overwrite</source>
+        <translation type="unfinished">Conferma la sovrascrittura</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="254"/>
+        <source>Writing to a physical device can corrupt the device.
+(Target Device: %1 &quot;%2&quot;)
+Are you sure you want to continue?</source>
+        <translation type="unfinished">Scrivere su un dispositivo fisico può corrompere il dispositivo.
+(Device di destinazione: %1 &quot;%2&quot;)
+Sei sicuro di voler continuare?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="325"/>
+        <source>Not enough space on disk: Size: %1 sectors  Available: %2 sectors  Sector size: %3</source>
+        <translation type="unfinished">Spazio su disco non sufficiente: Dimensione %1 settori  Disponibili: %2 settoti  Dimensione settore %3</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="404"/>
+        <location filename="mainwindow.cpp" line="409"/>
+        <location filename="mainwindow.cpp" line="414"/>
+        <location filename="mainwindow.cpp" line="428"/>
+        <source>File Error</source>
+        <translation type="unfinished">Errore file</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="404"/>
+        <source>The selected file does not exist.</source>
+        <translation type="unfinished">Il file selezionato non esiste.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="409"/>
+        <source>You do not have permision to read the selected file.</source>
+        <translation type="unfinished">Non hai permessi per leggere il file selezionato.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="414"/>
+        <source>The specified file contains no data.</source>
+        <translation type="unfinished">Il file selezionato non contiene dati.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="418"/>
+        <source>Done.</source>
+        <translation type="unfinished">Terminato.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="422"/>
+        <location filename="mainwindow.cpp" line="609"/>
+        <source>Complete</source>
+        <translation type="unfinished">Completo
+</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="422"/>
+        <source>Write Successful.</source>
+        <translation type="unfinished">Scrittura effettuata con successo.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="428"/>
+        <source>Please specify an image file to use.</source>
+        <translation type="unfinished">Per cortesia specifica un file di immagine da usare.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="449"/>
+        <source>Confirm Overwrite</source>
+        <translation type="unfinished">Conferma la sovrascrittua</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="449"/>
+        <source>Are you sure you want to overwrite the specified file?</source>
+        <translation type="unfinished">Sei sicuro di voler sovrascrivere il file specificato?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="526"/>
+        <source>Disk is not large enough for the specified image.</source>
+        <translation type="unfinished">Il disco non è grande a sufficienza per questo file di immagine.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="609"/>
+        <source>Read Successful.</source>
+        <translation type="unfinished">Lettura completata con successo.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="613"/>
+        <source>File Info</source>
+        <translation type="unfinished">Informazioni sul file</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="613"/>
+        <source>Please specify a file to save data to.</source>
+        <translation type="unfinished">Per cortesia seleziona un file dove salvare i dati.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="disk.cpp" line="51"/>
+        <location filename="disk.cpp" line="236"/>
+        <location filename="disk.cpp" line="325"/>
+        <location filename="disk.cpp" line="340"/>
+        <source>File Error</source>
+        <translation type="unfinished">Errore nel file</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="51"/>
+        <source>An error occurred when attempting to get a handle on the file.
+Error %1: %2</source>
+        <translation type="unfinished">Errore nell&apos;ottenere un handle per il file.
+Errore %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="70"/>
+        <location filename="disk.cpp" line="213"/>
+        <source>Device Error</source>
+        <translation type="unfinished">Errore sul dispositivo</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="71"/>
+        <source>An error occurred when attempting to get a handle on the device.
+Error %1: %2</source>
+        <translation type="unfinished">Errore nell&apos;ottenere un handle per il dispositivo.
+Errore %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="89"/>
+        <location filename="disk.cpp" line="417"/>
+        <source>Volume Error</source>
+        <translation type="unfinished">Errore sul volume</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="90"/>
+        <source>An error occurred when attempting to get a handle on the volume.
+Error %1: %2</source>
+        <translation type="unfinished">Errore nell&apos;ottenere un handle per il volume.
+Errore %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="107"/>
+        <source>Lock Error</source>
+        <translation type="unfinished">Errore ottenendo il lock sul volume</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="108"/>
+        <source>An error occurred when attempting to lock the volume.
+Error %1: %2</source>
+        <translation type="unfinished">Errore nell&apos;ottenere un lock per il volume.
+Errore %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="125"/>
+        <source>Unlock Error</source>
+        <translation type="unfinished">Errore eliminando il lock sul volume</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="126"/>
+        <source>An error occurred when attempting to unlock the volume.
+Error %1: %2</source>
+        <translation type="unfinished">Errore nel togliere il lock da un volume.
+Errore %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="143"/>
+        <source>Dismount Error</source>
+        <translation type="unfinished">Errore smontando il volume</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="144"/>
+        <source>An error occurred when attempting to dismount the volume.
+Error %1: %2</source>
+        <translation type="unfinished">Errore nelcercare di smontare il volume.
+Errore %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="171"/>
+        <source>Read Error</source>
+        <translation type="unfinished">Errore in lettura</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="172"/>
+        <source>An error occurred when attempting to read data from handle.
+Error %1: %2</source>
+        <translation type="unfinished">Errore nel leggere i dati da un handle.
+Errore %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="194"/>
+        <source>Write Error</source>
+        <translation type="unfinished">Errore in scrittura</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="195"/>
+        <source>An error occurred when attempting to write data to handle.
+Error %1: %2</source>
+        <translation type="unfinished">Errore nello scrivee i dati in un handle.
+Errore %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="214"/>
+        <source>An error occurred when attempting to get the device&apos;s geometry.
+Error %1: %2</source>
+        <translation type="unfinished">Errore nell&apos;acquisizione della geometria del device.
+Errore %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="237"/>
+        <source>An error occurred while getting the file size.
+Error %1: %2</source>
+        <translation type="unfinished">Errore nell&apos;ottenere la dimensione di un file.
+Errore %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="259"/>
+        <source>Free Space Error</source>
+        <translation type="unfinished">Non c&apos;è abbastanza spazio disponibile</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="260"/>
+        <source>Failed to get the free space on drive %1.
+Error %2: %3
+Checking of free space will be skipped.</source>
+        <translation type="unfinished">Errore nel leggere lo spazio disponibile sul disco %1.
+Errore %2: %3
+Il controllo dello spazio libero verrà saltato.</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="326"/>
+        <source>An error occurred while getting the device number.
+This usually means something is currently accessing the device;please close all applications and try again.
+
+Error %1: %2</source>
+        <translation type="unfinished">Errore nel leggere il numero del dispositivo.
+Di solito significa che qualcosa sta accedendo al dispositivo; per cortesia chiudi tutte le applicazioni e riprova.
+
+Errore %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="341"/>
+        <source>An error occurred while querying the properties.
+This usually means something is currently accessing the device; please close all applications and try again.
+
+Error %1: %2</source>
+        <translation type="unfinished">Errore nel leggerele proprietà.
+Di solito significa che qualcosa sta accedendo al dispositivo; per cortesia chiudi tutte le applicazioni e riprova.
+
+Errore %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="418"/>
+        <source>An error occurred when attempting to get a handle on %3.
+Error %1: %2</source>
+        <translation type="unfinished">Errore nell&apos;ottenere un handle per %3.
+Errore %1: %2</translation>
+    </message>
+</context>
+</TS>
